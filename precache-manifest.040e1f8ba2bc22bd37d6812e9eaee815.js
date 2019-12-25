540a0e4d62584c1da89874c2a9754366e0ab81ff
Abinavsudharshan <abinavsudharshan4699@gmail.com>
2019-12-25 15:18:59 +0530

    Updates

---
+++ precache-manifest.040e1f8ba2bc22bd37d6812e9eaee815.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c869a93e653e0fa34a844772e9dc983",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "315b099e0eed1f81cfe3",
+    "url": "/profile/static/css/main.4227110c.chunk.css"
+  },
+  {
+    "revision": "a9f96d84a5b525ad67cb",
+    "url": "/profile/static/js/2.c7e9ba8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.c7e9ba8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "315b099e0eed1f81cfe3",
+    "url": "/profile/static/js/main.51b9764a.chunk.js"
+  },
+  {
+    "revision": "f584263506eafd01872f",
+    "url": "/profile/static/js/runtime-main.e4c0ad69.js"
+  },
+  {
+    "revision": "d56e11e1773467b56976933d8c4075e8",
+    "url": "/profile/static/media/Abinav.d56e11e1.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
