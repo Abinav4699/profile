90c2f8c19eca36cf88c15aa1fd75477f0c05e960
Abinavsudharshan <abinavsudharshan4699@gmail.com>
2019-12-26 23:11:56 +0530

    Updates

---
+++ precache-manifest.d56d4fe7f9110fce033e56e179d8a511.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f1b49e85cb9ad5e8e9392fe0d3378b",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e11f3e1d84702d34af97",
+    "url": "/profile/static/css/main.4227110c.chunk.css"
+  },
+  {
+    "revision": "551b88217f223e6055c9",
+    "url": "/profile/static/js/2.6412a2b8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.6412a2b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e11f3e1d84702d34af97",
+    "url": "/profile/static/js/main.dc0f9645.chunk.js"
+  },
+  {
+    "revision": "f584263506eafd01872f",
+    "url": "/profile/static/js/runtime-main.e4c0ad69.js"
+  },
+  {
+    "revision": "d56e11e1773467b56976933d8c4075e8",
+    "url": "/profile/static/media/Abinav.d56e11e1.JPG"
+  },
+  {
+    "revision": "5e927c42827d0e7ab603243ee58c9ef0",
+    "url": "/profile/static/media/Abinav1.5e927c42.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.abbe854127f601b03d5bc37df10f1f7e.js"
+  "/profile/precache-manifest.d56d4fe7f9110fce033e56e179d8a511.js"
 );
 
 self.addEventListener('message', (event) => {
